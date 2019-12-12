3e3d29281f9c3351ede9b8d4cbbbc50ba11b9bdd
yaoxt3 <759741745@qq.com>
2019-12-12 22:58:12 +0800

    first commit

---
+++ include/lidar_main.h
@@ -0,0 +1,39 @@
+//
+// Created by yxt on 19-7-12.
+//
+
+#ifndef LIDAR_LIDAR_MAIN_H
+#define LIDAR_LIDAR_MAIN_H
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <algorithm>
+#include <ros/ros.h>
+#include <geometry_msgs/Vector3.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/filter.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/octree/octree.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+#endif //LIDAR_LIDAR_MAIN_H
