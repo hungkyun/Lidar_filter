7372e2aaab0c4fcc4badee0de8e4a328dfb3b5f0
yaoxt3 <759741745@qq.com>
2020-04-27 21:00:56 +0800

    333

--- include/Kalman.h
+++ include/Kalman.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
 #include <cmath>
 #include <vector>
 #include <ctime>
@@ -33,6 +34,7 @@ struct Vertex{
 	cv::Point right_point,left_point;
 	cv::Point top_point,low_point;
 	cv::Point mid_point;
+	cv::Point begin,end;
 	int flag;
 	double longth;
 	Vertex(){
@@ -44,22 +46,100 @@ struct Vertex{
 struct Target_{
 	int trace_ID;
 	int match;
-	cv::Mat param;
+	int point_num;
 	cv::Point target_point;
+	cv::Mat param;
 	cv::Mat pred;
 	cv::KalmanFilter kalman;
 	Target_(){
 		trace_ID = -1;
 		match = 0;
+		point_num = 0;
 	}
 	Vertex vertex;
 };
 
-struct curr_tar{
-	cv::Mat para;
-	int remove;
-	curr_tar(){
-		remove = 0;
-	}
+
+class KM{
+public:
+	KM();
+	bool dfs(int first);
+	void km_init();
+	double ikm_match();
+	const static int maxn=20;
+	const static int inf=999999;
+	bool vis_first[maxn];
+	bool vis_second[maxn];
+	double mmp[maxn][maxn];
+	double slack[maxn];
+	double val_first[maxn];
+	double val_second[maxn];
+	int match[maxn];
+	int num;
 };
 
+KM::KM(){
+	km_init();
+}
+
+void KM::km_init() {
+	num = 0;
+	memset(mmp,0,sizeof(mmp));
+	memset(match,0,sizeof(match));
+	memset(slack,0,sizeof(slack));
+	memset(match,-1,sizeof(match));
+	memset(val_first,0,sizeof(val_first));
+	memset(val_second,0,sizeof(val_second));
+	memset(vis_first,false,sizeof(vis_first));
+	memset(vis_second,false,sizeof(vis_second));
+}
+
+double KM::ikm_match() {
+	memset(match,-1,sizeof(match));
+	memset(val_second,0,sizeof(val_second));
+	for(int i=0;i<num;i++){
+		val_first[i]=mmp[i][0];
+		for(int j=1;j<num;j++){
+			val_first[i]=cv::max(val_first[i],mmp[i][j]);
+		}
+	}
+	for(int i=0;i<num;i++){
+		for (int k = 0; k < num; ++k) {
+			slack[k] = inf*1.0;
+		}
+		while(1){
+			memset(vis_first,false,sizeof(vis_first));
+			memset(vis_second,false,sizeof(vis_second));
+			if(dfs(i)) break;
+			double v=inf;
+			for(int j=0;j<num;j++)if(!vis_second[j]) v=cv::min(v,slack[j]);
+			for(int j=0;j<num;j++){
+				if(vis_first[j]) val_first[j]-=v;
+				if(vis_second[j]) val_second[j]+=v;
+				else slack[j]-=v;
+			}
+		}
+	}
+	double ans=0.0;
+	for(int i=0;i<num;i++) ans+=mmp[match[i]][i];
+	return ans;
+}
+
+bool KM::dfs(int first) {
+	vis_first[first]=true;
+	for(int j=0;j<num;j++){
+		if(vis_second[j]) continue;
+		double x=val_first[first]+val_second[j]-mmp[first][j];
+		if(abs(x)<0.0000001){
+			vis_second[j]=true;
+			if(match[j]==-1||dfs(match[j])){
+				match[j]=first;
+				return true;
+			}
+		}
+		else{
+			slack[j]=cv::min(slack[j],x);
+		}
+	}
+	return false;
+}
