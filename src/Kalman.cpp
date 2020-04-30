7baf791c46c087750426080795785504eaed2887
yaoxt3 <759741745@qq.com>
2020-04-30 16:48:26 +0800

    777

--- src/Kalman.cpp
+++ src/Kalman.cpp
@@ -24,6 +24,8 @@ private:
 	bool init;
 	double maxDis;
 	int track_num;
+	int frame;
+	int min_age;
 };
 
 Tracker::Tracker()
@@ -31,7 +33,9 @@ Tracker::Tracker()
 	tar_list.clear();
 	init = false;
 	track_num = 0;
-	maxDis = 10.0;
+	maxDis = 5.0;
+	frame = 0;
+	min_age = -2;
 	points_sub = nh.subscribe("all_points", 1028, &Tracker::processPointcloud, this);
 	predict_pub = nh.advertise<sensor_msgs::PointCloud2>("/predict_points", 10);
 	estimate_pub = nh.advertise<sensor_msgs::PointCloud2>("/estimated_points", 10);
@@ -44,7 +48,7 @@ void Tracker::processPointcloud(const sensor_msgs::PointCloud2 &scan)
 	pcl_conversions::toPCL(scan,pcl_pc);
 	pcl::PointCloud<pcl::PointXYZI>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZI>);
 	pcl::fromPCLPointCloud2(pcl_pc,*temp_cloud); // all points' data are stored in temp_cloud
-
+	std::cout << "frame: " << frame++ << std::endl;
 	std::cout << "point num: " << temp_cloud->points.size() << std::endl;
 	std::vector<pcl::PointIndices> cluster_indices;
 	clustering(temp_cloud,cluster_indices);
@@ -75,7 +79,7 @@ void Tracker::processPointcloud(const sensor_msgs::PointCloud2 &scan)
 		estimate_pub.publish(pub_estimated);
 	}
 	init = true;
-
+	cout << endl;
 }
 
 //least square solution
@@ -176,9 +180,9 @@ void Tracker::clustering(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud,std
 	pcl::search::KdTree<pcl::PointXYZI>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZI>);
 	tree->setInputCloud (in_cloud);
 	pcl::EuclideanClusterExtraction<pcl::PointXYZI> ec;
-	ec.setClusterTolerance (10);
-	ec.setMinClusterSize (20);
-	ec.setMaxClusterSize (2500);
+	ec.setClusterTolerance (5);
+	ec.setMinClusterSize (10);
+	ec.setMaxClusterSize (25000);
 	ec.setSearchMethod (tree);
 	ec.setInputCloud (in_cloud);
 	ec.extract (cluster_indices);
@@ -310,7 +314,7 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 	points.clear();
 	tar_now.clear();
 	disv.clear();
-
+	int count=0;
 	for (std::vector<pcl::PointIndices>::const_iterator i = cluster_indices.begin (); i != cluster_indices.end (); ++i)
 	{
 		std::vector<cv::Point> points_2d;
@@ -322,8 +326,10 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 			meanx += points_2d.back().x;
 			meany += points_2d.back().y;
 		}
+		cout << "cluster. " << count++ << " " << points_2d.size() << endl;
 		tar_temp.point_num = points_2d.size();
-		tar_temp.target_point = cv::Point(meanx/points_2d.size(),meany/points_2d.size());
+		tar_temp.target_point = cv::Point(meanx/(points_2d.size()*1.0),meany/(points_2d.size()*1.0));
+		cout << "tar_point: " << tar_temp.target_point.x << " " << tar_temp.target_point.y << endl;
 		getClusterVertex(points_2d,tar_temp.vertex);
 		polynomial_curve_fit(points_2d, 2, tar_temp.param);
 
@@ -332,19 +338,30 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 	}
 
 	// KM match
+	cout << "tar_now size: " << tar_now.size() << endl;
+	cout << "tar_list size: " << tar_list.size() << endl;
 	if(init)
 	{
+		cout << "--------------------" << endl;
 		//weight=n1/(d*n2), w1=last frame point num, w2=this frame point num
 		for (int i = 0; i < tar_now.size(); ++i)
 		{
+			cout << "now target: " << tar_now[i].target_point.x << " " << tar_now[i].target_point.y << " num: " << tar_now[i].point_num << endl;
+
 			for (int j = 0; j < tar_list.size(); ++j)
 			{
+				cout << "last target: " << tar_list[j].target_point.x << " " << tar_list[j].target_point.y << " num: " << tar_list[j].point_num << " age: " << tar_list[j].age<< endl;
 				double dis = sqrt(pow(tar_list[j].target_point.x-tar_now[i].target_point.x,2)+pow(tar_list[j].target_point.y-tar_now[i].target_point.y,2));
 				disv.push_back(dis);
-				ikm.mmp[i][j] = 1000.0 * tar_now[i].point_num / (tar_list[j].point_num * dis * 1.0);
+				//ikm.mmp[i][j] = 1000 * tar_now[i].point_num / (tar_list[j].point_num * dis * 1.0 + 1.0);
+				//ikm.mmp[i][j] =  1000.0 / (abs(tar_now[i].point_num - tar_list[j].point_num) + dis + 1.0);
+				//ikm.mmp[i][j] = 1000 * (1 - dis/maxDis) / sqrt(abs(tar_now[i].point_num-tar_list[j].point_num));
+				ikm.mmp[i][j] = 1000.0 / ((dis+1) * sqrt(abs(tar_now[i].point_num-tar_list[j].point_num)) * (1 - tar_list[j].age));
 				cout<< i << " " << j << " " << "distance: "<< dis <<endl;
+				cout << "weight: " << ikm.mmp[i][j] << endl;
 			}
 		}
+		cout << "--------------------" << endl;
 		//if n1 != n2, full mmp[][] with -1, optional step
 		if(tar_list.size()!=tar_now.size())
 		{
@@ -354,7 +371,7 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 				for (int i = tar_now.size(); i < tar_list.size(); ++i)
 				{
 					for (int j = 0; j < tar_list.size(); ++j) {
-						ikm.mmp[i][j] = -1.0;
+						ikm.mmp[i][j] = -5.0;
 					}
 				}
 				ikm.num = tar_list.size();
@@ -362,11 +379,9 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 			// expand column
 			else
 			{
-				for (int i = 0; i < tar_now.size(); ++i)
-				{
-					for (int j = tar_list.size(); j < tar_now.size(); ++j)
-					{
-						ikm.mmp[i][j] = -1.0;
+				for (int i = 0; i < tar_now.size(); ++i) {
+					for (int j = tar_list.size(); j < tar_now.size(); ++j) {
+						ikm.mmp[i][j] = -5.0;
 					}
 				}
 				ikm.num = tar_now.size();
@@ -375,11 +390,24 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 		else{
 			ikm.num = tar_now.size();
 		}
+
+		cout << "++++++++++++++++++++++++" << endl;
+		for (int l = 0; l < ikm.num; ++l) {
+			for (int i = 0; i < ikm.num; ++i) {
+				cout << ikm.mmp[l][i] << " ";
+			}
+			cout << endl;
+		}
+
 		double ans = ikm.ikm_match();
 		cout << "Ex. " << ans << endl;
-
+		for (int m = 0; m < ikm.num; ++m) {
+			cout << ikm.imatch[m] << " ";
+		}
+		cout << "++++++++++++++++++++++++" << endl;
+		cout << endl;
 		for (int k = 0; k < tar_now.size(); ++k) {
-			if(ikm.mmp[k][ikm.imatch[k]] < 0.000001)
+			if(ikm.mmp[k][ikm.imatch[k]] < 0)
 				tar_now[k].match = -1;
 			else{
 				int index = k*tar_list.size()+ikm.imatch[k];
@@ -457,7 +485,7 @@ void Tracker::tracking_match2(const pcl::PointCloud<pcl::PointXYZI>::Ptr in_clou
 			if(tar_list[i].match < 0){
 				tar_list[i].age--;
 			}
-			if(tar_list[i].age < -3){
+			if(tar_list[i].age < min_age){
 				tar_list.erase(tar_list.begin()+i);
 			}
 		}
@@ -482,30 +510,35 @@ int main(int argc, char **argv)
     */
     // test KM algorithm
 
-	/*
+
 	KM ikm;
-	ikm.num = 4;
-	ikm.mmp[0][0] = 0;
-	ikm.mmp[0][1] = 10;
-	ikm.mmp[0][2] = 0;
-	ikm.mmp[0][3] = 0;
-	ikm.mmp[1][0] = 0;
-	ikm.mmp[1][1] = 2;
-	ikm.mmp[1][2] = 0;
-	ikm.mmp[1][3] = 0;
-	ikm.mmp[2][0] = 0;
-	ikm.mmp[2][1] = 8;
-	ikm.mmp[2][2] = 0;
-	ikm.mmp[2][3] = 0;
-	ikm.mmp[3][0] = 0;
-	ikm.mmp[3][1] = 120;
-	ikm.mmp[3][2] = 0;
-	ikm.mmp[3][3] = 0;
+	ikm.km_init();
+	ikm.num = 5;
+	ikm.mmp[0][0] = 99.9171;
+	ikm.mmp[0][1] = 0.879795;
+	ikm.mmp[0][2] = 22.6896;
+	ikm.mmp[0][3] = 0.009008;
+	ikm.mmp[0][4] = 22.726;
+	ikm.mmp[1][0] = 0.168707;
+	ikm.mmp[1][1] = 100.284;
+	ikm.mmp[1][2] = 13.2233;
+	ikm.mmp[1][3] = 105800;
+	ikm.mmp[1][4] = 14.4516;
+	ikm.mmp[2][0] = -500;
+	ikm.mmp[2][1] = -500;
+	ikm.mmp[2][2] = -500;
+	ikm.mmp[2][3] = -500;
+	ikm.mmp[2][4] = -500;
+	ikm.mmp[3][0] = -500;
+	ikm.mmp[3][1] = -500;
+	ikm.mmp[3][2] = -500;
+	ikm.mmp[3][3] = -500;
+	ikm.mmp[3][4] = -500;
 	cout << ikm.ikm_match() << endl;
 	for (int i = 0; i < ikm.num; ++i) {
 		cout << ikm.imatch[i] << endl;
 	}
-	*/
+
     return 0;
 }
 
